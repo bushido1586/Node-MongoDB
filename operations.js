81580dbda979258c31ca455592d349323e064d0a
Bradley Reed <shenmusha@yahoo.com>
2021-08-31 20:29:20 -0400

    error fix

--- operations.js
+++ operations.js
@@ -2,7 +2,7 @@ const assert = require('assert').strict;
 
 exports.insertDocument = (db, document, collection, callback) => {
     const coll = db.collection(collection);
-    coll.insertOne(document, (err, result)=>{
+    coll.insertOne(document, (err, result) => {
         assert.strictEqual(err, null);
         callback(result);
     });
@@ -24,9 +24,9 @@ exports.removeDocument = (db, document, collection, callback) => {
     });
 };
 
-exports.UpdateDocument = (db, document, update, collection, callback) => {
+exports.updateDocument = (db, document, update, collection, callback) => {
     const coll = db.collection(collection);
-    coll.updateOne(document, { $set: update }, null, (err, result) =>{
+    coll.updateOne(document, { $set: update }, null, (err, result) => {
         assert.strictEqual(err, null);
         callback(result);
     });
